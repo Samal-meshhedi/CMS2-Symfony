f4cee95c0934b907d7026e7c516fc54301c526d2
christien-HP\christien <samalmeshhedi@gmail.com>
2017-02-20 14:46:45 +0100

    Initial commit;

---
+++ src/AppBundle/Controller/CmsController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Cms;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class CmsController extends Controller
+{
+    /**
+     * @Route("/", name="cms_list")
+     */
+    public function listAction(Request $request){
+        $cms2 = $this->getDoctrine()
+                ->getRepository('AppBundle:Cms')
+                ->findAll();
+                
+        return $this->render('cms/index.html.twig', array('cms2' => $cms2
+                
+        ));
+        
+    }
+    
+    /**
+     * @Route("cms/create", name="create_list")
+     */
+    public function createAction(Request $request){
+        $cms = new Cms;
+         
+        $form = $this->createFormBuilder($cms)
+                ->add('name', TextType::class, array('attr' => array('class' => 'form-control','style' => 'margin-bottom:15px')))
+                ->add('category', TextType::class, array('attr' => array('class' => 'form-control','style' => 'margin-bottom:15px')))
+                ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control','style' => 'margin-bottom:15px')))
+                ->add('priority', ChoiceType::class, array('choices' => array('Low' => 'low', 'Normal' => 'Normal', 'High' => 'High'), 'attr' => array('class' => 'form-control','style' => 'margin-bottom:15px')))
+                ->add('due_date', DateTimeType::class, array('attr' => array('class' => 'formcontrol','style' => 'margin-bottom:15px')))     
+                ->add('save', SubmitType::class, array('label' => 'Create Cms', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-bottom:15px')))
+                ->getForm();
+        
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+              //Get Date
+              $name = $form['name']->getData();
+              $category = $form['category']->getData();
+              $description = $form['description']->getData();
+              $priority = $form['priority']->getData();
+              $due_date = $form['due_date']->getData();
+              
+              $now = new\DateTime('now');
+              
+              $cms->setName($name);
+              $cms->setCategory($category);
+              $cms->setDescription($description);
+              $cms->setPriority($priority);
+              $cms->setDueDate($due_date);
+              $cms->setCreateDate($now);
+              
+              $em = $this->getDoctrine()->getManager();
+              
+              $em->persist($cms);
+              $em->flush();
+              
+              $this->addFlash(
+                    'notice',
+                    'cms Added'
+              );
+              
+              return $this->redirectToRoute('cms_list');
+              
+        }
+        
+        return $this->render('cms/create.html.twig', array(
+            'form' => $form->createView()
+        ));   
+    }
+    
+    /**
+     * @Route("cms/edit/{id}", name="edit_list")
+     */
+    public function editAction($id, Request $request){
+        return $this->render('cms/edit.html.twig');   
+    }
+    
+    /** 
+     * @Route("cms/details/{id}", name="details_list")
+     */
+    public function detailsAction($id){
+        $cms2 = $this->getDoctrine()
+                ->getRepository('AppBundle:Cms')
+                ->find($id);
+                
+        return $this->render('cms/details.html.twig', array('cms2' => $cms2));   
+    }
+}
---
+++ src/AppBundle/Entity/Cms.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Cms
+ *
+ * @ORM\Table(name="cms")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CmsRepository")
+ */
+class Cms
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="category", type="string", length=255)
+     */
+    private $category;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="priority", type="string", length=255)
+     */
+    private $priority;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="due_date", type="datetime")
+     */
+    private $dueDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="create_date", type="datetime")
+     */
+    private $createDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255)
+     */
+    private $username;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Cms
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set category
+     *
+     * @param string $category
+     *
+     * @return Cms
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return string
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Cms
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set priority
+     *
+     * @param string $priority
+     *
+     * @return Cms
+     */
+    public function setPriority($priority)
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    /**
+     * Get priority
+     *
+     * @return string
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+     * Set dueDate
+     *
+     * @param \DateTime $dueDate
+     *
+     * @return Cms
+     */
+    public function setDueDate($dueDate)
+    {
+        $this->dueDate = $dueDate;
+
+        return $this;
+    }
+
+    /**
+     * Get dueDate
+     *
+     * @return \DateTime
+     */
+    public function getDueDate()
+    {
+        return $this->dueDate;
+    }
+
+    /**
+     * Set createDate
+     *
+     * @param \DateTime $createDate
+     *
+     * @return Cms
+     */
+    public function setCreateDate($createDate)
+    {
+        $this->createDate = $createDate;
+
+        return $this;
+    }
+
+    /**
+     * Get createDate
+     *
+     * @return \DateTime
+     */
+    public function getCreateDate()
+    {
+        return $this->createDate;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return Cms
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+}
+
